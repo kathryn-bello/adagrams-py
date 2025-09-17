9260bc0e6719299665f5e929633cd863e1aa4f93
Kathryn <bello.kathrynj@gmail.com>
2025-09-17 07:45:47 -0400

    wave 3: score_board implementation

--- adagrams/game.py
+++ adagrams/game.py
@@ -56,7 +56,29 @@ def get_letter_pool(letter_list):
     return count_dict
 
 def score_word(word):
-    pass
+    # Letter to score mapping
+    letter_scores = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
+    
+    total_score = 0
+    
+    # If length of the word is 7, 8, 9, or 10, then the word gets an additional 8 points
+    if len(word) >= 7:
+        total_score += 8
+        
+    for letter in word.upper():
+        if letter in letter_scores:
+            total_score += letter_scores[letter]
+        
+    return total_score
+
 
 def get_highest_word_score(word_list):
     pass
